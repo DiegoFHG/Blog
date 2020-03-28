8ddd52fdca54d4e385a9a59052b0127848c88a0a
DiegoFranciscoHG <34254918+DiegoFranciscoHG@users.noreply.github.com>
2020-03-28 22:32:35 +0100

    Changed app base.

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,6 +1,5 @@
 module.exports = {
 	title: "Diego's boring blog",
-	base: '/Blog/',
 	head: [
 		['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' }],
 		['link', { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' }],
