2088f4b8a863628998385f1e66311e9ff6a61bee
DiegoFranciscoHG <34254918+DiegoFranciscoHG@users.noreply.github.com>
2020-03-28 16:24:46 +0100

    Configured favicons.

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,6 +1,13 @@
 module.exports = {
 	title: "Diego's boring blog",
 	base: '/Blog/',
+	head: [
+		['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' }],
+		['link', { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' }],
+		['link', { rel: "shortcut icon", href: "/favicon.ico"}],
+		['link', { rel: 'manifest', href: '/manifest.json' }],
+		['meta', { name: "msapplication-config", content: "/browserconfig.xml"}],
+	],
 	plugins: [
 		['@vuepress/back-to-top'],
 		[
